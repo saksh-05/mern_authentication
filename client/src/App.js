511cc86baa86689fea31359514cb0a302bc9e0ac
saurabhtopthon01 <55596640+saurabhtopthon01@users.noreply.github.com>
2021-09-18 17:27:13 +0530

    signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,23 @@
-import React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
-import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import Signup from './components/signup';
+import React from "react";
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Signup from "./components/signup";
+import { Button, Typography } from "@mui/material";
 
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
-function MyAppMode() {
-  const theme = useTheme();
-  const colorMode = React.useContext(ColorModeContext);
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        bgcolor: 'background.default',
-        color: 'text.primary',
-        borderRadius: 1,
-        p: 3,
-      }}
-    >
-      {theme.palette.mode} mode
-      <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
-    </Box>
-  );
-}
-
-
-
 const App = () => {
-  const [mode, setMode] = React.useState('light');
+  const [mode, setMode] = React.useState("light");
   const colorMode = React.useMemo(
     () => ({
       toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
       },
     }),
-    [],
+    []
   );
 
   const theme = React.useMemo(
@@ -52,18 +27,40 @@ const App = () => {
           mode,
         },
       }),
-    [mode],
+    [mode]
   );
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <MyAppMode />
+        <Box sx={{ bgcolor: "background.default" }}>
+          <Box>
+          <Button onClick={colorMode.toggleColorMode}>
+            {theme.palette.mode} mode
+            <IconButton sx={{ ml: 1 }} color="secondary">
+              {theme.palette.mode === "dark" ? (
+                <Brightness7Icon />
+              ) : (
+                <Brightness4Icon />
+              )}
+            </IconButton>
+          </Button>
+          </Box>
+          <Signup />
+          <Box
+            display="inline-flex"
+            justifyContent="left"
+            alignItems="center"
+            color="gray"
+            sx={{ m: "auto", width: "40%", ml: "23.8rem" }}
+          >
+            <Typography sx={{ flex: "1" }}>Create by Saurabh</Typography>
+            <Typography>devChallenges.io</Typography>
+          </Box>
+        </Box>
       </ThemeProvider>
-      <Signup />
     </ColorModeContext.Provider>
   );
-}
+};
 
-export default App
-
+export default App;
--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,21 +1,159 @@
-import React from "react";
-import { Card, Box, CardContent,Button } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Card,
+  Box,
+  CardContent,
+  Button,
+  TextField,
+  Typography,
+  FormControl,
+  InputAdornment,
+  IconButton,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+  Avatar,
+} from "@mui/material";
 import logo from "../resources/devchallenges.svg";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import EmailIcon from "@mui/icons-material/Email";
+import LockIcon from "@mui/icons-material/Lock";
+import Google from '../resources/Google.svg';
+import Facebook from '../resources/Facebook.svg';
+import Twitter from '../resources/Twitter.svg';
+import Github from '../resources/Github.svg';
 
-const signup = () => {
+const Signup = () => {
+  const [values, setValues] = useState({
+    password: "",
+    showpassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <Card sx={{ mx: "auto", width: "40%", height: "5rem" }}>
+      <Card sx={{ mx: "auto", width: "40%", p: 4 }}>
         <CardContent>
           <img src={logo} alt="logo" />
           <Box component="form">
-            <Button variant='contained'>Sign up for code </Button>
-            <Button>login</Button>
-            signup form</Box>
+            <Typography
+              variant="h5"
+              sx={{ fontWeight: "600", fontFamily: "Noto Sans", mt: 5 }}
+            >
+              Join thousands of learners from around the world
+            </Typography>
+            <Typography
+              sx={{ fontSize: "1rem", fontFamily: "Noto Sans", mt: 3, mb: 4 }}
+            >
+              Master web development by making real life projects. There are
+              multiple paths for you to choose
+            </Typography>
+            <FormControl sx={{ mb: 2, width: "100%" }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
+              <OutlinedInput
+                label="email"
+                id="outlined-adornment-email"
+                value={values.email}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <EmailIcon />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <FormControl sx={{ mb: 3, width: "100%" }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={values.showPassword ? "text" : "password"}
+                value={values.password}
+                onChange={handleChange("password")}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <LockIcon />
+                  </InputAdornment>
+                }
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+            <Button
+              sx={{
+                width: "100%",
+                fontFamily: "Noto Sans",
+                textTransform: "none",
+                fontSize: "1rem",
+              }}
+              variant="contained"
+            >
+              Start coding now{" "}
+            </Button>
+            <Typography
+              variant="caption"
+              display="block"
+              color='GrayText'
+              sx={{
+                fontSize: "14px",
+                fontFamily: "Noto Sans",
+                textAlign: "center",
+                mt: 5,
+                mb: 4,
+              }}
+            >
+              or continue with these social profile
+            </Typography>
+            <Stack direction='row' spacing={2} alignItems='center' justifyContent='center'>
+              <Avatar src={Google} alt='google' />
+              <Avatar src={Facebook} alt='facebook' />
+              <Avatar src={Twitter} alt='twitter' />
+              <Avatar src={Github} alt='github' />
+            </Stack>
+            <Typography
+              variant="caption"
+              display="block"
+              color='GrayText'
+              sx={{
+                fontSize: "14px",
+                fontFamily: "Noto Sans",
+                textAlign: "center",
+                mt: 5,
+              }}
+            >
+              Already a member?<a href='#'>Login</a>
+            </Typography>
+          </Box>
         </CardContent>
       </Card>
     </div>
   );
 };
 
-export default signup;
+export default Signup;
