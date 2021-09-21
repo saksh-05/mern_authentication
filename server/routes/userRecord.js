b101c6034c343cf846ad4956255b83639e311100
saurabhtopthon01 <55596640+saurabhtopthon01@users.noreply.github.com>
2021-09-21 21:05:22 +0530

    signup

--- server/routes/userRecord.js
+++ server/routes/userRecord.js
@@ -6,27 +6,92 @@ const userMiddleware = require("../middleware/userMiddleware");
 const bcrypt = require("bcrypt");
 
 router
-  .route("/")
+  .route("/signup")
   .post(userMiddleware(userValidationSchema.userInfo), (req, res) => {
     const saltRounds = 10;
     const userPassword = req.body.password;
     bcrypt.genSalt(saltRounds, function (err, salt) {
-      bcrypt.hash(userPassword, salt, function (err, hash) {
-        const newUserData = {
-          email: req.body.email,
-          password: hash,
-        };
-
-        const newUser = new User(newUserData);
-        newUser
-          .save()
-          .then(() =>
-            res.send({
-              status: true,
-              message: "user added",
-            }),
-          )
-          .catch((err) => res.status(400).json("Error: " + err));
+      bcrypt.hash(userPassword, salt, (err, hash) => {
+        User.findOne(
+          {
+            email: req.body.email,
+          },
+          (err, user) => {
+            console.log(user);
+            if (!user) {
+              console.log("new user");
+              const newUserData = {
+                email: req.body.email,
+                password: hash,
+              };
+              const newUser = new User(newUserData);
+              newUser
+                .save()
+                .then(() =>
+                  res.send({
+                    status: true,
+                    message: "user added",
+                  })
+                )
+                .catch((err) => res.status(400).json("Error: " + err));
+            } else if (err) console.log(err);
+            else {
+              bcrypt.compare(
+                req.body.password,
+                user.password,
+                (err, result) => {
+                  if (result == true) {
+                    console.log("user exist");
+                    res.send({
+                      status: true,
+                      message: "User Exist",
+                    });
+                  } else {
+                    res.send({
+                      status: false,
+                      message: "Incorrect password",
+                    });
+                    console.log("incorrect password");
+                  }
+                }
+              );
+            }
+          }
+        );
+        // .then((user) => {
+        //   console.log(user);
+        //   if (!user) {
+        //     console.log("new user");
+        //     const newUserData = {
+        //       email: req.body.email,
+        //       password: hash,
+        //     };
+        //     const newUser = new User(newUserData);
+        //     newUser
+        //       .save()
+        //       .then(() =>
+        //         res.send({
+        //           status: true,
+        //           message: "user added",
+        //         })
+        //       )
+        //       .catch((err) => res.status(400).json("Error: " + err));
+        //   } else {
+        //     bcrypt.compare(
+        //       req.body.password,
+        //       user.password,
+        //       function (err, result) {
+        //         if (result == true) {
+        //           console.log("user exist");
+        //         } else {
+        //           console.log(user.password);
+        //           console.log(hash);
+        //           res.send("Incorrect password");
+        //         }
+        //       }
+        //     );
+        //   }
+        // });
       });
     });
 
