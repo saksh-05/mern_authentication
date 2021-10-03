dde4c4eb9ca24b926a5cbc28e3f88aca506b29bf
saurabhtopthon01 <55596640+saurabhtopthon01@users.noreply.github.com>
2021-10-03 21:27:31 +0530

    devpro

--- client/src/devpro/Baseurl.jsx
+++ client/src/devpro/Baseurl.jsx
@@ -1,6 +1,6 @@
 const devURL = "http://localhost:3001/";
 
-const proURL = "";
+const proURL = "https://mern-authentication01.herokuapp.com/";
 
 const base_url = process.env.NODE_ENV === "production" ? proURL : devURL;
 
